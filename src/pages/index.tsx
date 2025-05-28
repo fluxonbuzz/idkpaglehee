d4ab24145bd2e5427293ce9c3fe2501c5658b191
fluxonbuzz <arnavkaundal90@gmail.com>
2025-05-29 00:43:20 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const featuredMods = [
   {
     title: "Crick Fusion",
     description: "Complete cricket gameplay overhaul with realistic physics",
-    image: "https://ik.imagekit.io/wjw1czp4c/WhatsApp%20Image%202025-05-28%20at%2022.31.36_6f635605.jpg", // Replace with your image URL
+    image: "assets/flux.png", // Replace with your image URL
     href: "",
   },
   {
