94223d811c170e8e3d893640d2e4dc5541a5a2f9
fluxonbuzz <arnavkaundal90@gmail.com>
2025-06-11 08:13:54 +0530

    Update signup.tsx

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,3 @@
-npm cache clean --forceimport Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import { Lock, Mail, Eye, User } from "lucide-react";
 import { useRef, useState } from "react";
