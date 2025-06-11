0f22d601b566b8247e6b4720fd513e72b092ed41
fluxonbuzz <arnavkaundal90@gmail.com>
2025-06-11 09:31:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,34 +1,37 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
-import WithPWA from "next-pwa";
+/** @type {import('next').NextConfig} */
+
+// Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+// This is especially useful for Docker builds.
+await import('./src/env.js');
+
+import WithPWA from 'next-pwa';
 
 const withPWA = WithPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
-  scope: "/",
-  sw: "service-worker.js",
+  scope: '/',
+  sw: 'service-worker.js',
 });
 
-/**
- * @type {import('next').NextConfig}
- */
-// @ts-ignore
-const config = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
-});
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
+  // If you are using `appDir` then you must comment the below `i18n` config out.
+  // @see https://github.com/vercel/next.js/issues/41980
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
 
-export default config;
+export default withPWA(nextConfig);
